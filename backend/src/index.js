ac3120f40085cb38d068a973ffc0a34106ece66f
Marcel Losso Forte <marcel.losso@gmail.com>
2020-03-28 23:06:40 -0300

    Finished all routes and controllerrs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
+// app.use(cors({
+//     origin: 'http://meuapp.com' // Endereço do frontend para indicar que apenas ele pode acessar o backend
+// }));
 app.use(express.json());
 app.use(routes);
 
